d1595564362622d6b232ec5593986dbed3736bee
sichan <sichan1301@naver.com>
2021-07-22 16:49:12 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
 import './App.css';
 
 function App() {
-  return <h1> Hello :)</h1>;
+  const [videos,setVideos] = useState([]); 
+
+
+  useEffect(()=>{     //마운트가 되었거나 업데이트가 될 때 콜백함수 등록
+  const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'   //fetch를 사용하면서 request를 할 때 옵션을 전달하는 것 
+  };
+
+  fetch("https://youtube.googleapis.com/youtube/v3\n/videos?key=AIzaSyCMEmeJkTqTovrbM6ryqVoF4qEX44S2IsA&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCMEmeJkTqTovrbM6ryqVoF4qEX44S2IsA"
+      , requestOptions)
+    .then(response => response.json())
+    .then(result => setVideos(result.items))
+    .catch(error => console.log('error', error));
+
+  },[]);
+  
+  return <VideoList videos = {videos} />;
 }
 
 export default App;
